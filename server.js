9e98c3392982a481f45a31ce15fa5e750b72a4ff
vilagen <vilagen@yahoo.com>
2020-01-20 14:08:31 -0500

    updated knox with mysql

--- server.js
+++ server.js
@@ -9,28 +9,38 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '',
+//     user : '',
+//     password : '',
+//     database : 'smartbrain'
+//   }
+// });
+
 const db = knex({
-  client: 'pg',
+  client: 'mysql',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host : 'localhost',
+    user : 'root',
+    password : 'password',
+    database : 'smartbrain'
   }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res)=> { res.send(db.users) });
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
 app.listen(3001, ()=> {
   console.log('app is running on port 3001');
-})
+});
