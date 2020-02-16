b3b8d853081f9a8406b81e1f66818ce74352acef
vilagen <vilagen@yahoo.com>
2020-02-16 14:27:18 -0500

    updated profile button.

--- smartbrain/src/components/Profile/ProfileIcon.js
+++ smartbrain/src/components/Profile/ProfileIcon.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class ProfileIcon extends React.Component {
   constructor(props) {
     super(props);
+    // this.toggle = this.toggle.bind(this) :the this keyword is used to get bind to the component.
     this.state = {
       dropdownOpen: false
     };
   };
 
+  toggle = () => { // arrow function implicitly binds the compononet or state.
+    this.setState(prevState => ({
+    dropdownOpen: !prevState.dropdownOpen
+    }));
+  };
+
   render() {
     return (
-      <div class="pa4 tc">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          class="br-100 ba h3 w3 dib" alt="avatar" />
+      <div className="pa4 tc">
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag="span"
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >      
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 ba h3 w3 dib" alt="avatar" 
+            />
+          </DropdownToggle>
+          <DropdownMenu className='b--transparent shadwo-5' 
+          style={{marginTop: '1px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>Sign Out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
       </div>
     )
   }
