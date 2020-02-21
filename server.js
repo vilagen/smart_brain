7fe6af26f076c73461b9f3cc6113e8aca7b69906
vilagen <vilagen@yahoo.com>
2020-02-21 07:39:10 -0500

    added new function for signing in.

--- server.js
+++ server.js
@@ -46,9 +46,10 @@ app.use(bodyParser.json());
 
 // app.get('/', (req, res)=> { res.send(db.users) });
 app.get('/', (req, res)=> { res.send("It's Working!") });
-app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -35,7 +35,9 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
+    joined: '',
+    pet: '',
+    age: '',
   }
 }
 
@@ -126,7 +128,7 @@ class App extends Component {
   };
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
     return (
       <div className="App">
          <Particles className='particles'
@@ -136,7 +138,11 @@ class App extends Component {
           toggleModal={this.toggleModal} />
           { isProfileOpen && 
             <Modal>
-              <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal}/>
+              <Profile 
+              isProfileOpen={isProfileOpen} 
+              toggleModal={this.toggleModal}
+              loadUser={this.loadUser}
+              user={user}/>
             </Modal>
           }
         { route === 'home'
