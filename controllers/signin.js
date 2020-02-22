a9ee129887afb49afddf20f2f250412e4fe0bdd6
vilagen <vilagen@yahoo.com>
2020-02-22 03:57:16 -0500

    Updating Authentication methods in signin.

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const redis = require("redis");
+
 // const handleSignin = (db, bcrypt, req, res) => {
 //   const { email, password } = req.body;
 //   if (!email || !password) {
@@ -21,6 +24,9 @@
 //     .catch(err => res.status(400).json('wrong credentials'))
 // }
 
+//setup Redis:
+const redisClient = redis.createClient(process.env.REDIS_URI);
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -33,7 +39,7 @@ const handleSignin = (db, bcrypt, req, res) => {
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(user => user[0] )
+          .then(user => user[0])
           .catch(err => Promise.reject('unable to get user'));
       } else {
         Promise.reject('wrong credentials');
@@ -42,19 +48,48 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch(err => res.status(400).json('wrong credentials'));
 };
 
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign( jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' } );
+};
 
-const getAuthTokenId = () => {
-  console.log('auth ok');
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value))
+};
+
+const createSessions = (user) => {
+  // JWT token, return user data
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() =>  { 
+      return {success: 'true', userId: id, token} 
+    })
+    .catch(console.log)
+};
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  redisClient.get(authorization, (err, reply) => {
+    if (err || !reply ) {
+      return res.status(400).json('Unauthorized');
+    }
+    return res.json({id: reply})
+  });
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.header;
-  return authorization ? getAuthTokenId() : 
+  return authorization ? getAuthTokenId(req, res) : 
     handleSignin(db, bcrypt, req, res)
-      .then(data => res.json(data))
+      .then(data => {
+         return data.id && data.email ? createSessions(data) :
+         Promise.reject(data)
+      })
+      .then(session => res.json(session))
       .catch(err => res.status(400).json(err));
 };
 
 module.exports = {
   signinAuthentication: signinAuthentication
-}
+};
--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', (req, res)=> { res.send("It's Working!") });
 app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
-app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)});
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
